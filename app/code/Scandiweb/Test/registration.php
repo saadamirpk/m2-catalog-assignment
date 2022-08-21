1626cee1df3147c98f72f94287d8874721349292
Saad Amir <59224810+saadamirpk@users.noreply.github.com>
2022-08-21 23:41:45 +0500

    fixed issues

--- app/code/Scandiweb/Test/registration.php
+++ app/code/Scandiweb/Test/registration.php
@@ -1,5 +1,5 @@
 <?php \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Scandiweb_Scandiweb_Test',
+	'Scandiweb_Test',
 	__DIR__
 );
